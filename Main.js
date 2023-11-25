ce4ed67c39284b745887d8a205424e80507bef8f
LB45000 <144392042+LB45000@users.noreply.github.com>
2023-11-24 20:23:00 -0800

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,9 @@
+
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+
+console.log('Subtraction:', subtract(5, 2));
+console.log('Multiplication:', multiply(3, 4));
+console.log('Division:', divide(8, 2));
